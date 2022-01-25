c407a365bc3b61f1a740acac3bb6829b7b9f83dc
vikram kumar <vikram.1rn14ec170@gmail.com>
2022-01-25 22:11:20 +0530

    update gateway with services deployed link

--- gateway.js
+++ gateway.js
@@ -1,27 +1,32 @@
-import { ApolloServer, gql } from 'apollo-server';
-import { ApolloGateway } from '@apollo/gateway';
+import { ApolloServer, gql } from "apollo-server";
+import { ApolloGateway } from "@apollo/gateway";
 
 const { PORT = 4000 } = process.env;
 
-
 (async () => {
-    // Initialize an ApolloGateway instance and pass it an array of services
-    // user and wallets api
-    const gateway = new ApolloGateway({
-      serviceList: [
-        { name: 'products', url: 'http://localhost:5001' },
-        { name: 'order', url: 'http://localhost:5002' }
-      ],
-    });
-  
-    // Pass the ApolloGateway to the ApolloServer constructor
-    const server = new ApolloServer({
-      gateway,
-      // Disable subscriptions (not currently supported with ApolloGateway)
-      subscriptions: false,
-    });
-  
-    server.listen({ port: PORT}).then(({ url }) => {
-      console.log(`🚀 Server ready at ${url}`);
-    });
-  })();
+  // Initialize an ApolloGateway instance and pass it an array of services
+  // user and wallets api
+  const gateway = new ApolloGateway({
+    serviceList: [
+      {
+        name: "products",
+        url: "https://fahion-town-order-service.herokuapp.com/",
+      },
+      {
+        name: "order",
+        url: "https://fashion-town-products-service.herokuapp.com/",
+      },
+    ],
+  });
+
+  // Pass the ApolloGateway to the ApolloServer constructor
+  const server = new ApolloServer({
+    gateway,
+    // Disable subscriptions (not currently supported with ApolloGateway)
+    subscriptions: false,
+  });
+
+  server.listen({ port: PORT }).then(({ url }) => {
+    console.log(`🚀 Server ready at ${url}`);
+  });
+})();
